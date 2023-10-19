c074d57d3b9c1e35d066ee0ce3a782310faeb86c
FabrizioCM <paulofabriziocm@gmail.com>
2023-10-19 17:08:33 -0600

    Tarea completa

--- filtro.js
+++ filtro.js
@@ -1,60 +1,63 @@
-// Tenemos un li de productos
 
 const productos = [
-  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./taco-negro.jpg"},
-  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./taco-azul.jpg"},
-  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./bota-negra.jpg"},
-  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./bota-azul.jpg"},
-  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./zapato-rojo.jpg"}
+  {nombre: "Zapato negro", tipo: "zapato", color: "negro", img: "./assets/taco-negro.jpg"},
+  {nombre: "Zapato azul", tipo: "zapato", color: "azul", img: "./assets/taco-azul.jpg"},
+  {nombre: "Bota negra", tipo: "bota", color: "negro", img: "./assets/bota-negra.jpg"},
+  {nombre: "Bota azul", tipo: "bota", color: "azul", img: "./assets/bota-azul.jpg"},
+  {nombre: "Zapato rojo", tipo: "zapato", color: "rojo", img: "./assets/zapato-rojo.jpg"}
 ]
 
-const li = document.getElementsByName("lista-de-productos")
-const $i = document.querySelector('.input');
+// Se cambio el nombre de las variables
+// Se cambio getElementByName por getElementById
+const listaProdudctos = document.getElementById("lista-de-productos")
+// Se quito el punto porque no hay clase input
+const input = document.querySelector('input');
 
 for (let i = 0; i < productos.length; i++) {
-  var d = document.createElement("div")
-  d.classList.add("producto")
+  var divProducto = document.createElement('div');
+  divProducto.classList.add('producto');
 
-  var ti = document.createElement("p")
-  ti.classList.add("titulo")
-  ti.textContent = productos[i].nombre
+  var titulo = document.createElement('p');
+  titulo.classList.add('titulo');
+  titulo.textContent = productos[i].nombre
   
-  var imagen = document.createElement("img");
+  var imagen = document.createElement('img');
   imagen.setAttribute('src', productos[i].img);
 
-  d.appendChild(ti)
-  d.appendChild(imagen)
+  divProducto.appendChild(titulo)
+  divProducto.appendChild(imagen)
 
-  li.appendChild(d)
+  listaProdudctos.appendChild(divProducto)
 }
 
-displayProductos(productos)
-const botonDeFiltro = document.querySelector("button");
+// se elimino porque no se ocupe
+// displayProductos(productos)
+const botonDeFiltro = document.querySelector('button');
 
 botonDeFiltro.onclick = function() {
-  while (li.firstChild) {
-    li.removeChild(li.firstChild);
+  while (listaProdudctos.firstChild) {
+    listaProdudctos.removeChild(listaProdudctos.firstChild);
   }
 
-  const texto = $i.value;
+  const texto = input.value;
   console.log(texto);
   const productosFiltrados = filtrado(productos, texto );
 
   for (let i = 0; i < productosFiltrados.length; i++) {
-    var d = document.createElement("div")
-    d.classList.add("producto")
+    var divProducto = document.createElement('div')
+    divProducto.classList.add('producto')
   
-    var ti = document.createElement("p")
-    ti.classList.add("titulo")
-    ti.textContent = productosFiltrados[i].nombre
+    var titulo = document.createElement('p')
+    titulo.classList.add('titulo')
+    titulo.textContent = productosFiltrados[i].nombre
     
-    var imagen = document.createElement("img");
+    var imagen = document.createElement('img');
     imagen.setAttribute('src', productosFiltrados[i].img);
   
-    d.appendChild(ti)
-    d.appendChild(imagen)
+    divProducto.appendChild(titulo)
+    divProducto.appendChild(imagen)
   
-    li.appendChild(d)
+    listaProdudctos.appendChild(divProducto)
   }
 }
 
